fd095c67a1d4e53a736420c9301909e5aae15616
Leandro-Pugliese <lean.pugliese@gmail.com>
2023-07-12 13:05:43 -0300

    Descargar QR Added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const info = document.querySelectorAll("#inputInfo");
 const boton = document.querySelectorAll("#BOTON");
 const boton_otro = document.querySelectorAll("#BOTON-OTRO");
+const boton_descargar = document.querySelectorAll("#BOTON-DESCARGAR");
 
-let data = ""
+let data = "";
 
 boton.forEach((boton) => {
     boton.addEventListener("click", () => {
@@ -21,6 +22,9 @@ boton.forEach((boton) => {
         boton_otro.forEach((boton_otro) => {
             boton_otro.style.display = "inline";
             boton.style.display = "none";
+            boton_descargar.forEach((boton_descargar) => {
+                boton_descargar.style.display = "inline"
+            });
             info.forEach((info) => {
                 info.style.display = "none";
             });
@@ -35,3 +39,20 @@ boton_otro.forEach((boton_otro) => {
     }
     boton_otro.addEventListener("click", nuevoQr);
 });
+
+
+boton_descargar.forEach((boton_descargar) => {
+    const descargarQr = () => {
+        html2canvas(document.getElementById("qrcode"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+                document.body.appendChild(canvas)
+                canvas.toBlob(function(blob) {
+                  saveAs(blob, "Qr.png")
+                });
+            }
+        });
+    }
+    boton_descargar.addEventListener("click", descargarQr);
+});
+
